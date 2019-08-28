c3ec7edcc0d610ba66264392ad45ee81ebf0aded
Stein <tarzanchemgio@gmail.com>
2019-08-28 17:51:31 +0700

    Play ground & Minions controller

---
+++ Assets/Scripts/TurnPoints.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnPoints : MonoBehaviour
+{
+    public static List<Transform> turnPoints = new List<Transform>();
+    private void Awake()
+    {
+        for(int i=0; i<this.transform.childCount; ++i)
+        {
+            turnPoints.Add(this.transform.GetChild(i));
+            //Debug.Log("Local pos: " + turnPoints[i].localPosition);
+            //Debug.Log("Real pos: " + turnPoints[i].position);
+        }
+    }
+}
---
+++ Assets/Scripts/WaveSpawningControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawningControl : MonoBehaviour
+{
+    public Transform spawningPoint;
+    public Transform minionPrefab;
+
+    public int totalWaveNumber = 3;
+    public float timeBetweenWaves = 3f;
+
+    float countDown = 2f;
+    int minionsNum = 1;
+
+    public Text countDownTimer;
+
+    private void Update()
+    {
+
+        if (countDown <= 0 && totalWaveNumber > 0)
+        {
+            StartCoroutine(spawnTheMinions(minionsNum));
+            //spawnTheMinions(minionsNum);
+            countDown = timeBetweenWaves;
+            minionsNum++;
+            timeBetweenWaves++;
+            totalWaveNumber--;
+        }
+
+        countDown -= Time.deltaTime;
+        if (countDown >= 0 && totalWaveNumber >0)
+        {
+            countDownTimer.text = Mathf.Round(countDown).ToString();
+        }
+      
+    }
+
+    IEnumerator spawnTheMinions(int minionNum)
+    {
+        for (int i = 0; i < minionNum; i++)
+        {
+            Instantiate(minionPrefab, spawningPoint.position, spawningPoint.rotation);
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
