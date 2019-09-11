f3ab4298163163c860bd14664f3821a1d821b773
Stein <tarzanchemgio@gmail.com>
2019-09-11 16:48:16 +0700

    Pause Menu & Continue Button

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseToggle();
+        }
+    }
+
+    public void pauseToggle()
+    {
+        if(pauseMenuUI.activeSelf)
+        {
+            pauseMenuUI.SetActive(!pauseMenuUI.activeSelf);
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            pauseMenuUI.SetActive(!pauseMenuUI.activeSelf);
+            Time.timeScale = 0f;
+        }
+    }
+
+}
